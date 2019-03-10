60e81939d98aa3a689e33b1e73d66079e460c60b
I'jaaz Tello <mitello@wccnet.edu>
2019-03-09 20:51:29 -0500

    forgot to add a new line

--- fahrenheit_to_centigrade.c
+++ fahrenheit_to_centigrade.c
@@ -10,7 +10,7 @@ main() {
 	step = 20; /* step size */
 
 	fahr = lower;
-	printf("Fahrenheit-Celsius");
+	printf("Fahrenheit-Celsius\n");
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);
 		printf("%3.0f %6.1f\n", fahr, celsius);
